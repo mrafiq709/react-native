b4d5cb010b38fb3ecf5dd5b2f2e546974bf28d62
RAFIQ <mrafiq709@gmail.com>
2020-10-01 00:34:44 +0700

    Welcome screen

--- App.js
+++ App.js
@@ -1,40 +1,6 @@
 import React from "react";
 import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "white",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: "dodgerblue",
-        }}
-      />
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: "gold",
-          top: 20,
-          left: 20,
-          position: "absolute", // relative
-        }}
-      />
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: "tomato",
-        }}
-      />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
