aa32ae86c048fcf28f3d43ef9d1cbf9da31df699
RAFIQ <mrafiq709@gmail.com>
2020-09-30 22:49:20 +0700

    flex-box

--- App.js
+++ App.js
@@ -1,45 +1,31 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableNativeFeedback,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-  Dimensions,
-} from "react-native";
-import { NativeModules } from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
 export default function App() {
-  //console.log(Dimensions.get("screen"));
-  //console.log(useDimensions());
-  //console.log(useDeviceOrientation());
-
-  const { landscape } = useDeviceOrientation();
   return (
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+      }}
+    >
       <View
         style={{
-          width: "100%",
-          height: landscape ? "100%" : "30%",
+          flex: 2,
           backgroundColor: "dodgerblue",
         }}
-      ></View>
-    </SafeAreaView>
+      />
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "gold",
+        }}
+      />
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "tomato",
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
