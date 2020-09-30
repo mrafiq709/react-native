d3b487466492cbc42a587e3170fb1491afdf9443
RAFIQ <mrafiq709@gmail.com>
2020-09-30 23:04:22 +0700

    flex-box align-ment

--- App.js
+++ App.js
@@ -7,19 +7,22 @@ export default function App() {
         flex: 1,
         backgroundColor: "white",
         flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <View
         style={{
           width: 100,
-          height: 100,
+          height: 300,
           backgroundColor: "dodgerblue",
+          alignSelf: "flex-start",
         }}
       />
       <View
         style={{
           width: 100,
-          height: 100,
+          height: 200,
           backgroundColor: "gold",
         }}
       />
