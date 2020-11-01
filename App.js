549bfdfed12e3ba40e6718180ba8768fa804ed9c
RAFIQ <mrafiq709@gmail.com>
2020-11-01 20:52:44 +0700

    styling text

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import {MaterialCommunityIcons} from '@expo/vector-icons';
@@ -12,34 +12,20 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          padding: 20,
-          paddingHorizontal: 10, // Override padding
-          paddingLeft: 30, // Override paddingHorizontal
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "gold",
-            height: 50,
-            width: 50,
-          }}
-        ></View>
-      </View>
-      <View
-        style={{
-          backgroundColor: "tomato",
-          height: 100,
-          width: 100,
-          margin: 20,
-          marginVertical: 10, // Override margin
-        }}
-      ></View>
-      
+     <Text
+      style={{
+        //fontFamily: "Roboto",
+        fontSize: 30,
+        fontStyle: "italic",
+        color: "tomato",
+        textTransform: "capitalize",
+        //textDecorationLine: "line-through",
+        textAlign: "center",
+        lineHeight: 30,
+      }}
+     >
+      i love react native. this is my first reactnative apps. here is some more text.
+     </Text> 
     </View>
   );
 }
