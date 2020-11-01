6c65a605d5253dee0231597e5854cb7e95b71be7
RAFIQ <mrafiq709@gmail.com>
2020-11-01 20:14:17 +0700

    expo icon: https://icons.expo.fyi/

--- App.js
+++ App.js
@@ -2,6 +2,17 @@ import React from "react";
 import { View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <MaterialCommunityIcons name="email" size={200} color="dodgerblue"/>
+    </View>
+  );
 }
