734266c195a8fb71a85ced42f7563adff57fb554
RAFIQ <mrafiq709@gmail.com>
2020-11-05 15:20:47 +0700

    Listing Details screen

--- App.js
+++ App.js
@@ -6,21 +6,10 @@ import {MaterialCommunityIcons} from '@expo/vector-icons';
 
 import AppButtn from './app/component/AppButton';
 import Card from "./app/component/Card";
+import ListingDetails from "./app/screens/ListingDetails";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 50,
-      }}
-    >
-      <Card
-        title="Red jacker for sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      ></Card>
-    </View>
+    <ListingDetails/>
   );
 }
---
+++ app/screens/ListingDetails.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Image, View, StyleSheet } from 'react-native';
+
+import AppText from '../component/AppText';
+import colors from '../config/colors';
+
+function ListingDetails(props) {
+    return (
+        <View>
+            <Image style={styles.image} source={require('../assets/jacket.jpg')}/>
+            <View style={styles.detailsContainer}>
+                <AppText style={styles.title}>Red Jacket for sale</AppText>
+                <AppText style={styles.price}>$100</AppText>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    detailsContainer: {
+        padding: 20,
+    },
+    image: {
+        width: "100%",
+        height: 300
+    },
+    price: {
+        color: colors.secondary,
+        fontWeight: "bold",
+        fontSize: 20,
+        marginVertical: 10
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "500",
+    }
+})
+
+export default ListingDetails;
